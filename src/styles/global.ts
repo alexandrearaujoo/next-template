d929e76af02939a8d278c51702bead6930313d97
Alexandre <alexandre01393@gmail.com>
2024-08-19 09:08:13 -0300

    add hooks folder

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,4 @@
-import styled, { createGlobalStyle } from 'styled-components';
-
-import { Toaster } from 'sonner';
+import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
     :root {
